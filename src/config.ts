ffd6f7eb403e7f739a294ff68d37653bb970b1f4
Emmanuel Chinda <97448054+GoodBoyC@users.noreply.github.com>
2026-07-31 15:49:14 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
+
 export const CONFIG = {
+  JSONBIN_API_KEY: '$2a$10$RnrCU7ULz1eBnJ5BtoFvj.uy5VToxEI6YaPUp2WCagJgPLvmM4Pe6', // Enter your JSONBin API key here
   JSONBIN_BIN_ID: '69e9dbc636566621a8e21c82',   // Enter your JSONBin Bin ID here
   USE_DEMO_STORAGE: false, // Set to false when using real JSONBin keys
 };
