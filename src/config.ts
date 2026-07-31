a39c92cb63f1c6fe917dfe4dee5bab014f7d5724
Emmanuel Chinda <97448054+GoodBoyC@users.noreply.github.com>
2026-07-31 15:41:54 -0300

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const CONFIG = {
+  JSONBIN_API_KEY: '', // Enter your JSONBin API key here
+  JSONBIN_BIN_ID: '',   // Enter your JSONBin Bin ID here
+  USE_DEMO_STORAGE: true, // Set to false when using real JSONBin keys
+};
